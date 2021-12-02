c660072a5bb00a7ac9504e1ff308f010593fb6ff
songdh99 <songdh99@gmail.com>
2021-12-02 22:39:35 +0900

    ver 1.02

--- src/self_drive.py
+++ src/self_drive.py
@@ -13,21 +13,22 @@ class SelfDrive:
 
 
     def lds_callback(self, scan):
-        front, front_left, front_right, left_side, right_side = self.init_ranges(scan.ranges)
-        if ((right_side * 2) / math.sqrt(3)) - 3 <= front_right <= ((right_side * 2) / math.sqrt(3)) + 3 and 23 < right_side < 27:
+        front, front_left, front_right, left_side, right_side, hyp2 = self.init_ranges(scan.ranges)
+        if hyp2 - 3 <= front_right <= hyp2 + 3 and 23 <= right_side <= 27:
             print("Go straight mode")
+            if right_side >= 25:
+                print("go around right")
+            elif right_side <= 25:
+                print("go around left")
+            else:
+                print("find walls")
 
         else:
-            print('front_right minimum', np.round((((right_side * 2) / math.sqrt(3)) - 3), 2))
-            print('front_right maximum', np.round((((right_side * 2) / math.sqrt(3)) + 3), 2))
+            print('front_right minimum', np.round(((hyp2) - 3), 2))
+            print('front_right maximum', np.round(((hyp2) + 3), 2))
             print('right_side minimum', right_side)
             print("Find walls")
-        if ((right_side * 2) / math.sqrt(3)) - 3 < front_right and 23 < right_side <27:
-            print("go around right")
-        elif (front_right < (right_side * 2)/ math.sqrt(3)) + 3 and 23< right_side < 27:
-            print("go around left")
-        else:
-            print("find straight")
+
 
         # if left <= 0.25:
         #     turtle_vel.linear.x = 0.15
@@ -55,9 +56,13 @@ class SelfDrive:
         left_side = scanned[90] * 100
         front = scanned[0] * 100
         front_right = scanned[-60] * 100
+        behind_right = scanned[-120] * 100
         front_left = scanned[60] * 100
+        hyp = (right_side * 2) / math.sqrt(3)
+        hyp2 = right_side / math.cos(30)
 
-        return np.round(front,2), np.round(front_left,2), np.round(front_right,2), np.round(left_side,2), np.round(right_side,2)
+        return np.round(front,2), np.round(front_left,2), np.round(front_right,2), np.round(left_side,2), np.round(right_side,2), hyp
+
 
 def main():
     rospy.init_node('self_drive')
