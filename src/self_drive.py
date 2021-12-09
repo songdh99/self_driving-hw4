18645ba7a676a1f40f7402b242c392b92ac27889
songdh99 <songdh99@gmail.com>
2021-12-10 01:44:59 +0900

    getting good

--- src/self_drive.py
+++ src/self_drive.py
@@ -10,118 +10,106 @@ import math
 class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
+        self.turtle_vel = Twist()
+        self.count = 0
 
+    def init_ranges(self, scan):
+        scanned = np.array(scan)
+        right = np.nan_to_num(np.mean(scanned[190:][np.nonzero(scanned[265:275])]))
+        fright = np.nan_to_num(np.mean(scanned[275:340][np.nonzero(scanned[340:360])]))
+        bright = np.nan_to_num(np.mean(scanned[245:265][np.nonzero(scanned[245:265])]))
+        front = np.nan_to_num(np.mean((scanned[340:360] + scanned[0:20])[np.nonzero(scanned[340:360] + scanned[0:20])]))
+        fleft = np.nan_to_num(np.mean(scanned[30:75][np.nonzero(scanned[30:75])]))
+        bleft = np.nan_to_num(np.mean(scanned[100:140][np.nonzero(scanned[100:140])]))
+        left = np.nan_to_num(np.mean(scanned[75:100][np.nonzero(scanned[75:100])]))
 
+        self.count += 1
+        return right, fright, front, fleft, left, bleft, bright
 
-    def init_ranges(self, scanned):
-        right_side = scanned[-90] * 100
-        left_side = scanned[90] * 100
-        front = scanned[0] * 100
-        front_right = scanned[-60] * 100
-        behind_right = scanned[-120] * 100
-        front_left = scanned[60] * 100
-        hyp = right_side / math.cos(math.pi*(30/180))
-        around = scanned[0:360] *100
+    def lds_callback(self, scan):
+        right, fright, front, fleft, left, bleft, bright = self.init_ranges(scan.ranges)
+        print(right, fright, front, fleft, left)
+        direct = 0
+        # if front > 3 and left > 3 and right > 3:
+        #     self.find_wall()
+        #     print("find wall")
+        #
+        # elif direct == 0:
+        #     if  left < 0.25 and right > 0.25:
+        #         direct = 1
+        #         print("left wall: ", left)
+        #
+        #     # elif right < 0.25 and right > 0.25:
+        #     #     direct = -1
+        #     #     print("right wall:", right*100)
+        #
+        #     else:
+        #         print("find the wall")
 
+        if direct == 0:  # 왼쪽 벽
 
-        right = scanned[-90:-60] * 100
-        fright = scanned[-30:-60] * 100
-        front = scanned[-30:30] * 100
-        fleft = scanned[30:60] * 100
-        left = scanned[60:90] * 100
+            if left > 0.25 and front > 0.25 or fleft > bleft:
+                self.Semi_Left()
+                print("lil turn left", self.Semi_Left())
 
-        return np.round(front,2), np.round(front_left,2), np.round(front_right,2), np.round(left_side,2), np.round(right_side,2), hyp, around
+            elif left < 0.25 and front > 0.25 or fleft < bleft:
+                self.Semi_Right()
+                print("lil turn right", self.Semi_Right())
 
-    def act_ros(self, scan):
-        global turtle_vel
-        turtle_vel = Twist()
-        front, front_left, front_right, left_side, right_side, hyp = self.init_ranges(scan.ranges)
-        if 25 < front < 50:
-            self.go()
-            if front_left < hyp and left_side < 25:
-                self.semi_turn_left()
-            elif front_left > hyp and left_side < 25:
-                self.semi_turn_left()
-            elif front_left < hyp and left_side > 25:
-                self.semi_turn_right()
-            elif front_left > hyp and left_side > 25:
-                self.semi_turn_right()
-        elif 0 <= front <= 25 and hyp-1 < front_left < hyp+1 and 26 < left_side < 27:
-            self.turn_right()
-        elif 50 <= front <= 100 and 50 <= front_left <= 100 and 50 <= left_side <= 100:
-            self.find_wall()
-        self.publisher.publish(turtle_vel)
+            elif front < 0.25:
+                self.Turn_Right()
+                print("turn left")
+            else:
+                print("no left wall")
 
+        # elif direct == -1:  #오른쪽 벽
+        #
+        #     if right > 0.25 and front > 0.25:
+        #         self.Semi_Left()
+        #         print("lil turnt left")
+        #
+        #     elif right < 0.25 and front > 0.25:
+        #         self.Semi_Right()
+        #         print("lil semi right")
+        #
+        #     elif front < 0.25:
+        #         self.Turn_Left()
+        #         print("turn left")
+        #     else:
+        #         print("no right wall")
 
-
-    def turn_left(self):
-        turtle_vel = Twist()
-        turtle_vel.linear.x = 0.05
-        turtle_vel.angular.z = 0.3
-        return turtle_vel
-    def semi_turn_left(self):
-        turtle_vel = Twist()
-        turtle_vel.linear.x = 0.15
-        turtle_vel.angular.z = 0.01
-        return turtle_vel
-    def semi_turn_right(self):
-        turtle_vel = Twist()
-        turtle_vel.linear.x = 0.15
-        turtle_vel.angular.z = -0.01
-        return turtle_vel
+        self.publisher.publish(self.turtle_vel)
 
     def find_wall(self):
-        turtle_vel =Twist()
-        turtle_vel.linear.x = 0.15
-        turtle_vel.angular.z = -0.03
-        return turtle_vel
-    def go(self):
-        turtle_vel = Twist()
-        turtle_vel.linear.x = 0.15
-        return turtle_vel
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.1
+        self.turtle_vel.angular.z = 0
 
+    def Turn_Right(self):
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.0
+        self.turtle_vel.angular.z = -0.3
 
+    def Follow_Wall(self):
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.15
+        self.turtle_vel.angular.z = 0
 
+    def Turn_Left(self):
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.0
+        self.turtle_vel.angular.z = 0.3
 
+    def Semi_Right(self):
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.195
+        self.turtle_vel.angular.z = -0.25
 
-
-
-    # def lds_callback(self, scan):
-    #     front, front_left, front_right, left_side, right_side, hyp = self.init_ranges(scan.ranges)
-    #     if hyp - 3 <= front_right <= hyp + 3 and 23 <= right_side <= 27:
-    #         print("Go straight mode")
-    #         if right_side >= 25 and front_right >= hyp:
-    #             print("go around right")
-    #         elif right_side < 25 and front_right <= hyp:
-    #             print("go around left")
-    #     else:
-    #         print('front_right minimum', np.round(((hyp) - 3), 2))
-    #         print('front_right maximum', np.round(((hyp) + 3), 2))
-    #         print('right_side minimum', right_side)
-    #         print("Find walls")
-    #     self.publisher.publish(turtle_vel)
-
-        # if left <= 0.25:
-        #     turtle_vel.linear.x = 0.15
-        # elif
-        #
-        #
-        # # scan 분석 후 속도 결정
-        # print("scan[0]:", scan.ranges[0])
-        # turtle_vel = Twist()
-        # # 전진 속도 및 회전 속도 지정
-        # turtle_vel.angular.z = 0.0
-        # # 장애물 발견시 정지
-        # # if scan.ranges[0] == 0.0:
-        # #     turtle_vel.linear.x = 0.15
-        # # elif scan.ranges[0] < 0.25:
-        # #     turtle_vel.linear.x = 0
-        # # else:
-        # #     turtle_vel.linear.x = 0.15
-        #
-        #  # 속도 출력
-        # self.publisher.publish(turtle_vel)
-
+    def Semi_Left(self):
+        self.turtle_vel = Twist()
+        self.turtle_vel.linear.x = 0.195
+        self.turtle_vel.angular.z = 0.25
+        # return turtle_vel
 
 
 def main():
@@ -129,8 +117,9 @@ def main():
     publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     driver = SelfDrive(publisher)
     subscriber = rospy.Subscriber('scan', LaserScan,
-                                  lambda scan: driver.act_ros(scan))
+                                  lambda scan: driver.lds_callback(scan))
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
--- src/wall_follow.py
+++ src/wall_follow.py
@@ -11,55 +11,74 @@ class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
         self.turtle_vel = Twist()
-
+        self.count = 0
 
     def init_ranges(self, scan):
         scanned = np.array(scan)
-        right = np.delete(np.mean(scanned[265:275]), 0)
-        fright = np.delate(np.mean(scanned[300:330]), 0)
-        front = np.mean(scanned[340:360]+scanned[0:20])
-        fleft = np.mean(scanned[30:60])
-        left = np.mean(scanned[85:95])
+        right = np.nan_to_num(np.mean(scanned[190:][np.nonzero(scanned[265:275])]))
+        fright = np.nan_to_num(np.mean(scanned[275:340][np.nonzero(scanned[340:360])]))
+        bright = np.nan_to_num(np.mean(scanned[245:265][np.nonzero(scanned[245:265])]))
+        front = scanned[0]
+        fleft = np.nan_to_num(np.mean(scanned[30:75][np.nonzero(scanned[30:75])]))
+        bleft = np.nan_to_num(np.mean(scanned[105:140][np.nonzero(scanned[105:140])]))
+        left = np.nan_to_num(np.mean(scanned[75:105][np.nonzero(scanned[75:105])]))
 
-        return right, fright, front, fleft, left
+        self.count += 1
+        return right, fright, front, fleft, left, bleft, bright
 
     def lds_callback(self, scan):
-        right, fright, front, fleft, left = self.init_ranges(scan.ranges)
-        print(right, fright, front, fleft, left)
+        right, fright, front, fleft, left, bleft, bright = self.init_ranges(scan.ranges)
+        print(front)
         direct = 0
-        if front > 2 and left > 2 and right > 2:
+        # if front > 3 and left > 3 and right > 3:
+        #     self.find_wall()
+        #     print("find wall")
+        #
+        # elif direct == 0:
+        #     if  left < 0.25 and right > 0.25:
+        #         direct = 1
+        #         print("left wall: ", left)
+        #
+        #     # elif right < 0.25 and right > 0.25:
+        #     #     direct = -1
+        #     #     print("right wall:", right*100)
+        #
+        #     else:
+        #         print("find the wall")
+
+
+        if front < 0.25 :
+            self.Turn_Right()
+            print("turn right")
+
+
+        elif left > 0.25 or fleft > bleft:
+            self.Semi_Left()
+            print("lil turn left", self.Semi_Left())
+
+        elif left < 0.25 or fleft < bleft:
+             self.Semi_Right()
+             print("lil turn right", self.Semi_Right())
+
+        else:
+             self.find_wall()
+             print("no left wall")
+
+
+        if left > 0.25 and fleft > bleft:
+            self.Semi_Left()
+            print("semi turn")
+
+        elif left < 0.25 or fleft < bleft:
+            self.Semi_Right()
+            print("semi right")
+
+        elif fleft > 0.5 or fleft == 0:
+            self.Turn_Left()
+            print("turn left")
+
+        else:
             self.find_wall()
-            print("find wall")
-        elif scan.ranges[0:360] == 0:
-            self.Follow_Wall()
-
-        elif direct == 0:
-            if left < 0.25 and right > 0.25:
-                direct = 1
-                print("left wall: ", left*100)
-
-            # elif right < 0.25 and right > 0.25:
-            #     direct = -1
-            #     print("right wall:", right*100)
-
-            else:
-                print("find the wall")
-
-        if direct == 1:  #왼쪽 벽
-
-            if left > 0.25 and front > 0.25:
-                self.Semi_Left()
-                print("lil turn left", self.Semi_Left())
-
-            elif left < 0.25 and front > 0.25:
-                self.Semi_Right()
-                print("lil turn right", self.Semi_Left())
-
-            elif front < 0.25:
-                self.Turn_Right()
-                print("turn left", left)
-            else:
-                print("no left wall")
 
         # elif direct == -1:  #오른쪽 벽
         #
@@ -76,9 +95,8 @@ class SelfDrive:
         #         print("turn left")
         #     else:
         #         print("no right wall")
-	
-        self.publisher.publish(self.turtle_vel)
 
+        self.publisher.publish(self.turtle_vel)
 
     def find_wall(self):
         self.turtle_vel = Twist()
@@ -88,7 +106,7 @@ class SelfDrive:
     def Turn_Right(self):
         self.turtle_vel = Twist()
         self.turtle_vel.linear.x = 0.0
-        self.turtle_vel.angular.z = -0.3
+        self.turtle_vel.angular.z = -2
 
     def Follow_Wall(self):
         self.turtle_vel = Twist()
@@ -97,18 +115,18 @@ class SelfDrive:
 
     def Turn_Left(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.0
-        self.turtle_vel.angular.z = 0.3
+        self.turtle_vel.linear.x = 0.15
+        self.turtle_vel.angular.z = 2
 
     def Semi_Right(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.1
-        self.turtle_vel.angular.z = -0.3
+        self.turtle_vel.linear.x = 0.15
+        self.turtle_vel.angular.z = -0.25
 
     def Semi_Left(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.1
-        self.turtle_vel.angular.z = 0.3
+        self.turtle_vel.linear.x = 0.15
+        self.turtle_vel.angular.z = 0.25
         # return turtle_vel
 
 
@@ -119,6 +137,7 @@ def main():
     subscriber = rospy.Subscriber('scan', LaserScan,
                                   lambda scan: driver.lds_callback(scan))
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
