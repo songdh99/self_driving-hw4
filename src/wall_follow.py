07ff525bf3cbcb0591c6db185fc6e4235973f418
songdh99 <songdh99@gmail.com>
2021-12-13 21:29:05 +0900

    perfect when inside

--- src/wall_follow.py
+++ src/wall_follow.py
@@ -19,114 +19,81 @@ class SelfDrive:
         fright = np.nan_to_num(np.mean(scanned[275:340][np.nonzero(scanned[340:360])]))
         bright = np.nan_to_num(np.mean(scanned[245:265][np.nonzero(scanned[245:265])]))
         front = scanned[0]
-        fleft = np.nan_to_num(np.mean(scanned[30:75][np.nonzero(scanned[30:75])]))
-        bleft = np.nan_to_num(np.mean(scanned[105:140][np.nonzero(scanned[105:140])]))
-        left = np.nan_to_num(np.mean(scanned[75:105][np.nonzero(scanned[75:105])]))
+        fleft = np.nan_to_num(np.mean(scanned[1:45][np.nonzero(scanned[1:45])]))
+        left = np.nan_to_num(np.mean(scanned[45:135][np.nonzero(scanned[75:105])]))
+        bleft = np.nan_to_num(np.mean(scanned[135:180][np.nonzero(scanned[145:180])]))
 
-        self.count += 1
         return right, fright, front, fleft, left, bleft, bright
 
     def lds_callback(self, scan):
         right, fright, front, fleft, left, bleft, bright = self.init_ranges(scan.ranges)
         print(front)
-        direct = 0
-        # if front > 3 and left > 3 and right > 3:
-        #     self.find_wall()
-        #     print("find wall")
-        #
-        # elif direct == 0:
-        #     if  left < 0.25 and right > 0.25:
-        #         direct = 1
-        #         print("left wall: ", left)
-        #
-        #     # elif right < 0.25 and right > 0.25:
-        #     #     direct = -1
-        #     #     print("right wall:", right*100)
-        #
-        #     else:
-        #         print("find the wall")
 
-        if right <1.5:
-            if front < 0.25 :
+        if right < 1:
+            if front < 0.30:
                 self.Turn_Right()
                 print("turn right")
 
+            elif left > 0.25:
+                if fleft > bleft:
+                    self.Semi_Left()
+                    print("lil turn left", self.Semi_Left())
+                elif fleft < bleft:
+                    self.Semi_Right()
+                    print("lil turn right", self.Semi_Right())
 
-            elif left > 0.25 or fleft > bleft:
-                self.Semi_Left()
-                print("lil turn left", self.Semi_Left())
 
-            elif left < 0.25 or fleft < bleft:
-                 self.Semi_Right()
-                 print("lil turn right", self.Semi_Right())
+            elif left < 0.25:
+                if fleft < bleft:
+                    self.Turn_Right()
+                    print("lil turn right", self.Semi_Right())
+
+                else:
+                    self.Semi_Left()
+                    print("lil turn left", self.Semi_Left())
 
             else:
                  self.find_wall()
                  print("no left wall")
 
-        elif right > 1:
-            if fleft > 0.5 or fleft == 0:
-                self.Turn_Left()
-                print("turn left")
-
-            elif left < 0.25 or fleft < bleft:
-                self.Semi_Right()
-                print("semi right")
-
-            elif left > 0.25 and fleft > bleft:
-                self.Semi_Left()
-                print("semi turn")
-
-            else:
-                self.find_wall()
-
-        # elif direct == -1:  #오른쪽 벽
-        #
-        #     if right > 0.25 and front > 0.25:
-        #         self.Semi_Left()
-        #         print("lil turnt left")
-        #
-        #     elif right < 0.25 and front > 0.25:
-        #         self.Semi_Right()
-        #         print("lil semi right")
-        #
-        #     elif front < 0.25:
+        # elif front > 1.5:
+        #     if fleft > 0.5 or fleft == 0:
         #         self.Turn_Left()
         #         print("turn left")
+        #
+        #     elif left < 0.25 or fleft < bleft:
+        #         self.Semi_Right()
+        #         print("semi right")
+        #
+        #     elif left > 0.25 and fleft > bleft:
+        #         self.Semi_Left()
+        #         print("semi turn")
+        #
         #     else:
-        #         print("no right wall")
+        #         self.find_wall()
+        #
 
         self.publisher.publish(self.turtle_vel)
 
-    def find_wall(self):
-        self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.1
-        self.turtle_vel.angular.z = 0
-
     def Turn_Right(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.0
-        self.turtle_vel.angular.z = -2
-
-    def Follow_Wall(self):
-        self.turtle_vel = Twist()
         self.turtle_vel.linear.x = 0.15
-        self.turtle_vel.angular.z = 0
+        self.turtle_vel.angular.z = -2.5
 
     def Turn_Left(self):
         self.turtle_vel = Twist()
         self.turtle_vel.linear.x = 0.15
-        self.turtle_vel.angular.z = 2
+        self.turtle_vel.angular.z = 3
 
     def Semi_Right(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.15
-        self.turtle_vel.angular.z = -0.25
+        self.turtle_vel.linear.x = 0.2
+        self.turtle_vel.angular.z = -1.15
 
     def Semi_Left(self):
         self.turtle_vel = Twist()
-        self.turtle_vel.linear.x = 0.15
-        self.turtle_vel.angular.z = 0.25
+        self.turtle_vel.linear.x = 0.2
+        self.turtle_vel.angular.z = 0.5
         # return turtle_vel
 
 
